3b65e56b6c7acf3f45fd9b7662d1b8feee4b3c9f
Julien Khamis <julien@julienpsh.local>
2023-03-07 12:40:19 +0100

    Initial commit

---
+++ Models/StandardArchive.cs
@@ -0,0 +1,9 @@
+using Piranha.AttributeBuilder;
+using Piranha.Models;
+
+namespace piranhademo.Models;
+
+[PageType(Title = "Standard archive", IsArchive = true)]
+public class StandardArchive : Page<StandardArchive>
+{
+}
